997be052bd10870718765fd090db540d4995d5ce
kopniiiin <kopniiiin@icloud.com>
2020-07-06 06:45:21 +0300

    Помещает настройки babel в отдельный файл

--- webpack.config.js
+++ webpack.config.js
@@ -17,15 +17,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: `babel-loader`,
-          options: {
-            presets: [
-              [`@babel/preset-env`, {targets: {node: `current`}}],
-              `@babel/preset-react`
-            ]
-          }
-        }
+        use: {loader: `babel-loader`}
       },
       {
         test: /\.scss$/,
